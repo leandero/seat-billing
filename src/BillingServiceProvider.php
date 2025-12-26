338396c4fd0f35828bb4c0ed76da09a20a4380b0
leandero <leandero@msn.com>
2025-12-26 17:33:27 -0600

    Fix namespace declaration in BillingServiceProvider

--- src/BillingServiceProvider.php
+++ src/BillingServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace leandero\Seat\Billing;;
+namespace leandero\Seat\Billing;
 
 use Denngarr\Seat\Billing\Commands\BillingUpdateLive;
 use Denngarr\Seat\Billing\database\seeders\ScheduleSeeder;
